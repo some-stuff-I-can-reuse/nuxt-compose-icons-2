29e542b62bbcc0cba8df5254c52709ede254c656
Arthur Plazanet <plazanet.art@gmail.com>
2024-09-19 16:04:52 +0200

    Fix layout pictures

--- playground/pages/why-literal-strings-to-create-vue-components.vue
+++ playground/pages/why-literal-strings-to-create-vue-components.vue
@@ -28,15 +28,23 @@
 
 .wlstcvc {
   display: flex;
-  // align-items: center;
+  flex-direction: column;
+  align-items: center;
   justify-content: center;
   min-height: 100svh;
-  padding: 1.5rem;
+  padding: 1rem;
+  gap: 1.5rem;
+
+  @include media('desktop-xl') {
+    padding: 1.5rem;
+    flex-direction: row;
+    flex-wrap: wrap;
+    align-items: flex-start;
+  }
 
   .why_are_you_looking_at_the_code_of_this_stupid_page {
     max-height: 100%;
     flex: 1;
-    margin: 0 1rem;
 
     .wlstcvc-img {
       max-width: 100%;
