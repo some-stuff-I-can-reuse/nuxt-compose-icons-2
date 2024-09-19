31cab281fd16e49352e7e693ac953d2a55e19163
Arthur Plazanet <plazanet.art@gmail.com>
2024-09-19 15:04:47 +0200

    Favicon and weird interaction between module options and playground in this setup

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -22,6 +22,7 @@ export default defineNuxtConfig({
       // <meta name="theme-color" content="#ffffff">
       link: [
         // favicon https://realfavicongenerator.net/
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
         { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
         { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
         { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
